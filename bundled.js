fcde1885f2f5ecf5903d1859ec4b63ddef60f3a3
Sunil Hari <isunilhari@gmail.com>
2017-11-25 10:03:29 +0530

    added bundled.js to gitignore

--- bundled.js
+++ bundled.js
@@ -8,7 +8,15 @@
  */
 
 var mountNode = document.getElementById('container');
-function build(type, attributes) {
+var attributeMap = {
+    "className": "class"
+    /**
+     * Triggered by the JSX Parser.
+     * @param {*String} type
+     * @param {*Object} attributes
+     * @param {*Array} children
+     */
+};function build(type, attributes) {
     for (var _len = arguments.length, children = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
         children[_key - 2] = arguments[_key];
     }
@@ -25,7 +33,7 @@ function build(type, attributes) {
 function see() {
     return build(
         "div",
-        { id: "app" },
+        { id: "app", className: "first-class" },
         build(
             "div",
             { id: "one" },
@@ -38,16 +46,49 @@ function see() {
         )
     );
 }
-
+/**
+ * Sets the attributes to a dom node
+ * @param {*node} el
+ * @param {*array} attributes
+ */
+function setProperties(el, attributes) {
+    Object.keys(attributes).forEach(function (name) {
+        setProperty(el, name, attributes[name]);
+    });
+}
+/**
+ * Sets a single attribute value
+ * @param {*node} el
+ * @param {*String} attribute
+ * @param {*any} value
+ */
+function setProperty(el, attribute, value) {
+    el.setAttribute(mapAttributes(attribute), value);
+}
+/**
+ *Converts JSX attribues such as className to class.
+ * @param {*String} name 
+ */
+function mapAttributes(name) {
+    return attributeMap[name] ? attributeMap[name] : name;
+}
+/**
+ * Creates the dom elements recursively.
+ * @param {*object} node
+ */
 function create(node) {
     if (typeof node === "string") {
         return document.createTextNode(node);
     }
     var el = document.createElement(node.type);
+    setProperties(el, node.attributes);
     node.children.map(create).forEach(el.appendChild.bind(el));
 
     return el;
 }
+/**
+ * renders the elements to the mount node
+ */
 function render() {
     mountNode.append(create(see(0)));
 }
